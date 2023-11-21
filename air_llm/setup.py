35536c888a06b5c7d85c74b96d6944160d767ece
Yu Li <lyo.gavin@gmail.com>
2023-11-20 20:52:01 -0600

    version

--- air_llm/setup.py
+++ air_llm/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="airllm",
-    version="0.9.4",
+    version="0.9.5",
     author="Gavin Li",
     author_email="gavinli@animaai.cloud",
     description="AirLLM allows single 4GB GPU card to run 70B large language models without quantization, distillation or pruning.",
